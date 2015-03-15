4fa12c17815031c5a6eb808fd617698d7b0499f0
Scott Michaud <scott@loosescrew.ca>
2015-03-15 16:13:07 -0400

    World Map Is Now Defined
    A 2D canvas illustrates the simulation world. A boid was created to
    represent the listener position and direction.
    
    World Map Is Now Defined

---
+++ classes/boid.js
@@ -0,0 +1,92 @@
+/*
+* A Simple Boid Class
+* Author: Scott Michaud
+* License: Proprietary (so I can re-license later)
+* Copyright 2015
+*/
+
+/*
+* It's a data structure to create a boid.
+* Points are defined as (x,y) pairs on an eight-element array.
+* Pairs are listed clockwise: Forward, Starboard, Rear, Port.
+* Origin is the rear point.
+*/
+
+(function () {
+  "use strict";
+  window.boid = function (size) {
+    this.size = size;
+    this.halfWidth = this.size * 0.5;
+    this.location = {x: 0, y: 0};
+    this.target = {x: 0, y: 1};
+    this.points = [0, 0, 0, 0, 0, 0, 0, 0];
+    
+    this.defineBoid();
+  };
+  
+  window.boid.prototype.setCenter = function (x, y) {
+    this.location.x = x;
+    this.location.y = y;
+    
+    //this.defineBoid();
+  };
+  
+  window.boid.prototype.setTarget = function (x, y) {
+    this.target.x = x;
+    this.target.y = y;
+    
+    //this.defineBoid();
+  };
+  
+  window.boid.prototype.defineBoid = function () {
+    var direction,
+      normalizeFactor,
+      rotatedDirection;
+    
+    direction = {
+      x: this.target.x - this.location.x,
+      y: this.target.y - this.location.y
+    };
+    
+    normalizeFactor = Math.sqrt(
+      Math.pow(direction.x, 2) + Math.pow(direction.y, 2)
+    );
+    
+    direction.x = direction.x / normalizeFactor;
+    direction.y = direction.y / normalizeFactor;
+    
+    this.points[4] = this.location.x;
+    this.points[5] = this.location.y;
+    
+    //75% of the size goes forward. 25% goes backwards.
+    this.points[0] = (this.size * 0.75 * direction.x) + this.location.x;
+    this.points[1] = (this.size * 0.75 * direction.y) + this.location.y;
+    
+    /*
+    * 90 deg to the right is:
+    *                         [0 -1]
+    *                         [1  0]
+    */
+    
+    rotatedDirection = {
+      x: -direction.y,
+      y: direction.x
+    };
+    
+    //Wings
+    //Move 25% of total size away from origin, in minus-forward direction.
+    //Then move half-of-a-width orthogonal to forward.
+    
+    //Starboard wing
+    this.points[2] = this.location.x - (this.size * 0.25 * direction.x);
+    this.points[2] = this.points[2] + (rotatedDirection.x * this.halfWidth);
+    this.points[3] = this.location.y - (this.size * 0.25 * direction.y);
+    this.points[3] = this.points[3] + (rotatedDirection.y * this.halfWidth);
+    
+    //Port wing
+    this.points[6] = this.location.x - (this.size * 0.25 * direction.x);
+    this.points[6] = this.points[6] - (rotatedDirection.x * this.halfWidth);
+    this.points[7] = this.location.y - (this.size * 0.25 * direction.y);
+    this.points[7] = this.points[7] - (rotatedDirection.y * this.halfWidth);
+  };
+}());
--- main.js
+++ main.js
@@ -16,14 +16,33 @@ app.init = function () {
   
   app.map = document.getElementById('map');
   app.drawCtx = app.map.getContext('2d');
-  app.map.w = document.getElementById('main').clientWidth;
-  app.map.h = document.getElementById('main').clientHeight;
+  app.map.width = document.getElementById('main').clientWidth;
+  app.map.height = document.getElementById('main').clientHeight;
 };
 
 app.start = function () {
   "use strict";
+  
   app.drawCtx.fillStyle = "#fff";
-  app.drawCtx.fillRect(0, 0, app.map.w, app.map.h);
+  app.drawCtx.fillRect(0, 0, app.map.width, app.map.height);
+  
+  app.boid = new window.boid(40);
+  app.boid.setCenter(app.map.width / 2, app.map.height / 2);
+  app.boid.setTarget(app.map.width / 2, (app.map.height / 2) - 1);
+  app.boid.defineBoid();
+  
+  app.drawCtx.beginPath();
+  app.drawCtx.moveTo(app.boid.points[0], app.boid.points[1]);
+  app.drawCtx.lineTo(app.boid.points[2], app.boid.points[3]);
+  app.drawCtx.lineTo(app.boid.points[4], app.boid.points[5]);
+  app.drawCtx.lineTo(app.boid.points[6], app.boid.points[7]);
+  app.drawCtx.fillStyle = "#000";
+  app.drawCtx.fill();
+  
+  app.drawCtx.beginPath();
+  app.drawCtx.arc(app.boid.location.x, app.boid.location.y, 4, 0, 2 * Math.PI);
+  app.drawCtx.fillStyle = "red";
+  app.drawCtx.fill();
 };
 
 window.addEventListener('DOMContentLoaded', app.init, false);

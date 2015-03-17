903b1628dd58bd8904e1551c8364e30dc9d3e040
Scott Michaud <scott@loosescrew.ca>
2015-03-16 20:07:28 -0400

    Allow Boid Movement and Reorientation

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ app.init = function () {
   app.drawCtx = app.map.getContext('2d');
   app.map.width = document.getElementById('main').clientWidth;
   app.map.height = document.getElementById('main').clientHeight;
+  app.map.addEventListener("mousedown", app.canvasLMBDown, false);
+  app.map.addEventListener("mouseup", app.canvasLMBUp, false);
 };
 
 app.start = function () {
@@ -57,18 +59,53 @@ app.start = function () {
   app.boid.setTarget(app.map.width / 2, (app.map.height / 2) - 1);
   app.boid.defineBoid();
   
-  app.drawCtx.beginPath();
-  app.drawCtx.moveTo(app.boid.points[0], app.boid.points[1]);
-  app.drawCtx.lineTo(app.boid.points[2], app.boid.points[3]);
-  app.drawCtx.lineTo(app.boid.points[4], app.boid.points[5]);
-  app.drawCtx.lineTo(app.boid.points[6], app.boid.points[7]);
-  app.drawCtx.fillStyle = '#000';
-  app.drawCtx.fill();
+  app.boid.draw = function () {
+    app.startFrame();
+    app.drawCtx.beginPath();
+    app.drawCtx.moveTo(app.boid.points[0], app.boid.points[1]);
+    app.drawCtx.lineTo(app.boid.points[2], app.boid.points[3]);
+    app.drawCtx.lineTo(app.boid.points[4], app.boid.points[5]);
+    app.drawCtx.lineTo(app.boid.points[6], app.boid.points[7]);
+    app.drawCtx.fillStyle = '#000';
+    app.drawCtx.fill();
+
+    app.drawCtx.beginPath();
+    app.drawCtx.arc(app.boid.location.x, app.boid.location.y, 4, 0, 2 * Math.PI);
+    app.drawCtx.fillStyle = 'red';
+    app.drawCtx.fill();
+  };
+  app.boid.draw();
+};
+
+app.startFrame = function () {
+  "use strict";
   
-  app.drawCtx.beginPath();
-  app.drawCtx.arc(app.boid.location.x, app.boid.location.y, 4, 0, 2 * Math.PI);
-  app.drawCtx.fillStyle = 'red';
-  app.drawCtx.fill();
+  app.drawCtx.fillStyle = '#fff';
+  app.drawCtx.fillRect(0, 0, app.map.width, app.map.height);
+};
+
+app.canvasLMBDown = function (e) {
+  "use strict";
+  var x,
+    y;
+  
+  x = e.clientX - 75;
+  y = e.clientY - 75;
+  
+  app.boid.setCenter(x, y);
+};
+
+app.canvasLMBUp = function (e) {
+  "use strict";
+  var x,
+    y;
+  
+  x = e.clientX - 75;
+  y = e.clientY - 75;
+  
+  app.boid.setTarget(x, y);
+  app.boid.defineBoid();
+  app.boid.draw();
 };
 
 app.noOffloadSelected = function () {

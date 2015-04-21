9d7dfd69bcf5a883db58af68338b40f3e82fcc33
Scott Michaud <scott@loosescrew.ca>
2015-04-21 17:40:49 -0400

    Possibly Scrapped. Rethinking WebCL Loading

--- main.js
+++ main.js
@@ -28,22 +28,16 @@ app.init = function () {
   app.rain.decay = 500; //Milliseconds
   app.rain.probability = 1 / ((app.rain.decay / 1000) * 60);
   app.rain.timestep = 512; //Not relevant for WebAudio, just WebCL.
-  app.rain.bOutputFirstBuffer = true;
-  app.rain.firstBuffer = new window.Float32Array(app.rain.timestep * 2);
-  app.rain.secondBuffer = new window.Float32Array(app.rain.timestep * 2);
-  app.rain.spawnDelay = [];
-  app.webclDistanceScale = 1000;
+  app.rain.outBuffer = window.Float32Array(app.rain.timestep * 2);
+  app.webclDistanceScale = 100;
   
-  app.elMap = document.getElementById('main'); //Container for various canvas layers.
-  
+  app.elMap = document.getElementById('main');
   app.elWebAudio = document.getElementById('typeWebAudio');
   app.elWebCL = document.getElementById('typeWebCL');
   app.elDeviceSelector = document.getElementById('webclDeviceSelector');
-  
   app.elStart = document.getElementById('startStop');
+  app.elLoad = document.getElementById('load');
   app.elRain = document.getElementById('glPoints');
-  app.elRain.width = app.elMap.clientWidth;
-  app.elRain.height = app.elMap.clientHeight;
   
   try {
     app.gl = app.elRain.getContext('webgl') || app.elRain.getContext('experimental-webgl');
@@ -72,8 +66,6 @@ app.init = function () {
   
   app.elBoid = document.getElementById('boid');
   app.c2d = app.elBoid.getContext('2d');
-  app.elBoid.width = app.elMap.clientWidth;
-  app.elBoid.height = app.elMap.clientHeight;
   app.elMap.addEventListener('mousedown', app.canvasLMBDown, false);
   app.elMap.addEventListener('mouseup', app.canvasLMBUp, false);
   app.elMap.addEventListener('mousemove', app.canvasMouseMove, false);
@@ -100,10 +92,15 @@ app.load = function () {
     app.elWebCL.setAttribute('disabled', 'disabled');
   }
   
-  //Set "No Offloading" as default
   app.elDeviceSelector.setAttribute('disabled', 'disabled');
   app.elWebCL.removeAttribute('checked');
   app.elWebAudio.setAttribute('checked', 'checked');
+  
+  app.elRain.width = app.elMap.clientWidth;
+  app.elRain.height = app.elMap.clientHeight;
+  app.gl.viewport(0, 0, app.gl.drawingBufferWidth, app.gl.drawingBufferHeight);
+  app.elBoid.width = app.elMap.clientWidth;
+  app.elBoid.height = app.elMap.clientHeight;
   
   sampleLoader = new window.AudioSampleLoader();
   sampleLoader.src = 'assets/Water-Dirt-01.ogg';
@@ -135,7 +132,6 @@ app.load = function () {
     app.c2d.fill();
   };
   
-  app.firstDraw();
   window.requestAnimationFrame(app.draw);
 };
 
@@ -202,51 +198,30 @@ app.parseInputParticleCount = function () {
   }
 };
 
-app.beginPress = function () {
+app.loadPress = function () {
   "use strict";
-  var preventCrash;
   
-  app.bIsRunning = true;
-  app.elStart.textContent = 'Stop';
-  app.elStart.onclick = app.stopPress;
-  
-  app.lastFrame = window.performance.now();
-  
-  app.parseInputParticleCount();
-  
-  app.generateInitialParticles(app.rain.max);
-  
-  if (app.elWebCL.hasAttribute('checked')) {
-    app.clGetter.selectProcessor(app.elDeviceSelector.selectedIndex);
-    app.getKernel();
-    app.setupKernel();
-    
-    preventCrash = function () {
-      app.scriptNode = app.audio.createScriptProcessor(app.rain.timestep, 2, 2);
-      app.scriptNode.onaudioprocess = app.scriptAudioCallback;
-      app.scriptNode.connect(app.audio.destination);
-    };
-    
-    window.setTimeout(preventCrash, 2000);
-  }
+  app.clGetter.selectProcessor(app.elDeviceSelector.selectedIndex);
+  app.getKernel();
+  app.initWebCL();
 };
 
-app.stopPress = function () {
+app.beginPress = function () {
   "use strict";
   
-  app.bIsRunning = false;
-  app.elStart.textContent = 'Begin';
-  app.elStart.onclick = app.beginPress;
-  app.gl.clear(app.gl.COLOR_BUFFER_BIT);
+};
+
+app.pingReady = function () {
+  "use strict";
   
-  if (app.scriptNode) {
-    app.scriptNode.disconnect();
-  }
-  if (app.clGetter.clCtx) {
-    window.setTimeout(function () {
-      app.clGetter.clCtx.releaseAll();
-    }, ((app.rain.timestep + 100) * 1000 / 44100));
-  }
+  //Only need to wait for sample(s) load/decode to complete.
+  app.unlockBegin();
+};
+
+app.unlockBegin = function () {
+  "use strict";
+  
+  app.elStart.removeAttribute('disabled');
 };
 
 app.selectNoOffload = function () {
@@ -265,38 +240,11 @@ app.selectWebCL = function () {
   app.elWebAudio.removeAttribute('checked');
 };
 
-app.pingReady = function () {
-  "use strict";
-  
-  //Only need to wait for sample(s) load/decode to complete.
-  app.unlockBegin();
-};
-
-app.unlockBegin = function () {
-  "use strict";
-  
-  app.elStart.removeAttribute('disabled');
-};
-
-app.firstDraw = function () {
-  "use strict";
-  
-  app.elRain.width = app.elMap.clientWidth;
-  app.elRain.height = app.elMap.clientHeight;
-  app.gl.viewport(0, 0, app.gl.drawingBufferWidth, app.gl.drawingBufferHeight);
-  app.elBoid.width = app.elMap.clientWidth;
-  app.elBoid.height = app.elMap.clientHeight;
-  
-  app.boid.defineBoid();
-  app.boid.draw();
-};
-
 app.draw = function (timestamp) {
   "use strict";
   
   window.requestAnimationFrame(app.draw);
   
-  //pragma region Simulating Rain
   if (app.bIsRunning) {
     
     if (app.elWebAudio.hasAttribute('checked')) {
@@ -307,9 +255,7 @@ app.draw = function (timestamp) {
     
     app.rain.draw();
   }
-  //pragma endregion
   
-  //pragma region Animating Boids
   if (app.boid.bNeedsRedraw) {
     app.c2d.clearRect(0, 0, app.elBoid.width, app.elBoid.height);
     app.boid.setTarget(app.mouse.x, app.mouse.y);
@@ -321,9 +267,6 @@ app.draw = function (timestamp) {
       app.boid.bNeedsRedraw = false;
     }
   }
-  //pragma endregion
-  
-  //pragma region Animating Resize
   if (app.bWasResized) {
     
     app.bWasResized = false;
@@ -340,153 +283,7 @@ app.draw = function (timestamp) {
     app.boid.defineBoid();
     app.boid.draw();
   }
-  //pragma endregion
-  
   app.lastFrame = timestamp;
-};
-
-app.generateInitialParticles = function (number) {
-  "use strict";
-  var i,
-    cueSamples,
-    cuePosition;
-  
-  //app.rain.max = number;
-  
-  //Web Audio: AOS: {float x, float y, float life}
-  //WebCL: AOS: {float x, float y, int32 sampleStart, int32 sampleEnd, int32 samplePosition}
-  //CHANGE: Now two: {float x, float y} & {int32 sampleStart, int32 sampleEnd, int32 samplePosition}
-  //Structures are not allowed in kernel params for WebCL.
-    
-  app.rain.webAudioCalls = new window.ArrayBuffer(number * 12);
-  app.rain.webclCallsFloat = new window.ArrayBuffer(number * 8);
-  app.rain.webclCallsInt = new window.ArrayBuffer(number * 12);
-  
-  app.rain.webAudioView = new window.Float32Array(app.rain.webAudioCalls);
-  
-  app.rain.webclFloatView = new window.Float32Array(app.rain.webclCallsFloat);
-  app.rain.webclIntView = new window.Int32Array(app.rain.webclCallsInt);
-  
-  cueSamples = app.rain.samples.length;
-  
-  for (i = 0; i < 3 * app.rain.max; i += 1) {
-    app.rain.webAudioView[i] = Math.random();
-  }
-  
-  //Replacement for below.
-  for (i = 0; i < app.rain.max; i += 1) {
-    app.rain.webclFloatView[2 * i] = app.rain.webAudioView[3 * i] * app.webclDistanceScale;
-    app.rain.webclFloatView[2 * i + 1] = app.rain.webAudioView[3 * i + 1] * app.webclDistanceScale;
-    
-    app.rain.webclIntView[3 * i] = 0; //TODO: Multiple Samples
-    app.rain.webclIntView[3 * i + 1] = cueSamples;
-    cuePosition = app.rain.webAudioView[3 * i + 2] * Math.round(44100 * app.rain.decay / 1000);
-    app.rain.webclIntView[3 * i + 2] = cuePosition;
-  }
-};
-
-app.webAudioSimulateRain = function (timestamp) {
-  "use strict";
-  var i,
-    deltaTime,
-    audioTime,
-    srcNode,
-    pannerNode;
-  
-  audioTime = app.audio.currentTime;
-  app.audio.listener.setPosition(app.boid.location.x / app.elBoid.width,
-                                 app.boid.location.y / app.elBoid.height,
-                                 0);
-  app.audio.listener.setOrientation(-app.boid.direction.x,
-                                    -app.boid.direction.y,
-                                    0,
-                                    0, 0, 1);
-  
-  deltaTime = timestamp - app.lastFrame;
-  for (i = 0; i < 3 * app.rain.max; i += 3) {
-    app.rain.webAudioView[i + 2] -= deltaTime / app.rain.decay;
-    
-    //Triggers a new sound call.
-    if (app.rain.webAudioView[i + 2] <= 0 && app.rain.probability > Math.random()) {
-      app.rain.webAudioView[i] = Math.random();
-      app.rain.webAudioView[i + 1] = Math.random();
-      app.rain.webAudioView[i + 2] = 1;
-      
-      srcNode = app.audio.createBufferSource();
-      srcNode.buffer = app.rain.samples;
-      pannerNode = app.audio.createPanner();
-      pannerNode.panningModel = 'equalpower';
-      pannerNode.setPosition(app.rain.webAudioView[i],
-                             app.rain.webAudioView[i + 1],
-                             0);
-      srcNode.connect(pannerNode);
-      pannerNode.connect(app.audio.destination);
-      srcNode.start(audioTime + (0.03 * Math.random()));
-    }
-  }
-};
-
-app.webclSimulateRainAudio = function (e) {
-  "use strict";
-  var i;
-  
-  for (i = 0; i < app.rain.max; i += 1) {
-    //If "life" has been reset: start a new sound.
-    //Else: Keep simulating current one.
-    
-    if (app.rain.webAudioView[3 * i + 2] === 1) {
-      app.rain.webclFloatView[2 * i] = app.rain.webAudioView[3 * i] * app.webclDistanceScale;
-      app.rain.webclFloatView[2 * i + 1] = app.rain.webAudioView[3 * i + 1] * app.webclDistanceScale;
-      
-      //Set the current sample to the start.
-      app.rain.webclIntView[3 * i + 2] = app.rain.webclIntView[3 * i];
-      //TODO: Adjust sampleStart (3i) and sampleEnd (2i + 1) for new sound cue, if multiple.
-      app.rain.webAudioView[3 * i + 2] = 0.99;
-      
-    } else {
-      app.rain.webclIntView[3 * i + 2] += app.rain.timestep;
-    }
-  }
-  
-  //response = app.runKernel();
-  
-  //outputFullBuffer = e.outputBuffer;
-  //outputLeft = outputFullBuffer.getChannelData(0);
-  //outputRight = outputFullBuffer.getChannelData(1);
-  
-  //for (i = 0; i < 2 * app.rain.timestep; i += 2) {
-  //  outputLeft[i / 2] = response[i];
-  //  outputRight[i / 2] = response[i + 1];
-  //}
-};
-
-app.webclSimulateRainVideo = function (timestamp) {
-  "use strict";
-  var i,
-    deltaTime;
-  
-  deltaTime = timestamp - app.lastFrame;
-  
-  for (i = 0; i < 3 * app.rain.max; i += 3) {
-    app.rain.webAudioView[i + 2] -= deltaTime / app.rain.decay;
-    
-    //Triggers a new sound call.
-    if (app.rain.webAudioView[i + 2] <= 0 && app.rain.probability > Math.random()) {
-      app.rain.webAudioView[i] = Math.random();
-      app.rain.webAudioView[i + 1] = Math.random();
-      app.rain.webAudioView[i + 2] = 1;
-    }
-  }
-};
-
-app.rain.draw = function () {
-  "use strict";
-  
-  app.gl.clear(app.gl.COLOR_BUFFER_BIT);
-  app.gl.bindBuffer(app.gl.ARRAY_BUFFER, app.vtxRainBuffer);
-  app.gl.bufferData(app.gl.ARRAY_BUFFER, app.rain.webAudioView, app.gl.STATIC_DRAW);
-  app.gl.vertexAttribPointer(app.vtxPositionAttribute, 3, app.gl.FLOAT, false, 0, 0);
-  app.gl.drawArrays(app.gl.POINTS, 0, app.rain.max);
 };
 
 app.resize = function () {
@@ -495,123 +292,23 @@ app.resize = function () {
   app.bWasResized = true;
 };
 
-app.getKernel = function () {
+app.getKernelSrc = function () {
   "use strict";
   
   app.kernelSrc = document.getElementById('clMixSamples').text;
 };
 
-app.setupKernel = function () {
+//Runs when device is selected, but not number of particles.
+app.initWebCL = function () {
   "use strict";
-  var cl;
   
-  cl = app.clGetter.clCtx;
-  app.bufParticlesFloat = cl.createBuffer(window.WebCL.MEM_READ_ONLY, app.rain.webclCallsFloat.byteLength);
-  app.bufParticlesInt = cl.createBuffer(window.WebCL.MEM_READ_ONLY, app.rain.webclCallsInt.byteLength);
-  app.bufOutput = cl.createBuffer(window.WebCL.MEM_WRITE_ONLY, 8 * app.rain.timestep);
-  app.bufSoundCues = cl.createBuffer(window.WebCL.MEM_READ_ONLY, app.rain.samples.getChannelData(0).byteLength);
-  app.clProgram = cl.createProgram(app.kernelSrc);
-  app.device = cl.getInfo(window.WebCL.CONTEXT_DEVICES)[0];
-  app.clProgram.build([app.device], "");
-  app.kernel = app.clProgram.createKernel('clMixSamples');
-  app.kernel.setArg(0, app.bufParticlesFloat);
-  app.kernel.setArg(1, app.bufParticlesInt);
-  app.kernel.setArg(2, app.bufOutput);
-  app.kernel.setArg(3, app.bufSoundCues);
-  app.kernel.setArg(6, new window.Int32Array([app.rain.max]));
-  //Each sound sample is 4 bytes.
-  app.kernel.setArg(7, new window.Int32Array([app.rain.samples.getChannelData(0).byteLength / 4]));
-  app.cmdQueue = app.clGetter.clCtx.createCommandQueue(app.device);
-  app.cmdQueue.enqueueWriteBuffer(app.bufSoundCues, false, 0, app.rain.samples.getChannelData(0).byteLength, app.rain.samples.getChannelData(0));
-  app.output = new window.Float32Array(app.rain.timestep * 2);
 };
 
-app.scriptAudioCallback = function (e) {
-  "use strict";
-  var i,
-    leftChannel,
-    rightChannel;
-  
-  //Read the last audio frame.
-  app.cmdQueue.enqueueReadBuffer(app.bufOutput, false, 0, app.output.byteLength, app.output);
-  app.cmdQueue.finish();
-  
-  if (app.rain.bOutputFirstBuffer) {
-    app.rain.firstBuffer = app.output;
-  } else {
-    app.rain.secondBuffer = app.output;
-  }
-  
-  //Get new particles
-  for (i = 0; i < app.rain.spawnDelay.length; i += 1) {
-    if (app.rain.spawnDelay[i] !== 0) {
-      //Queue a new rain sample delayed by this amount.
-      
-    }
-  }
-  
-  
-  //Blank array now that we know what we need to spawn
-  app.rain.spawnDelay.fill(0);
-  
-  //Kick off WebCL event. Make it affect the next callback.
-  //window.setTimeout(app.runKernel, 0);
-  //This might not be possible, because of how the browser schedules
-  //onaudioprocess callbacks.
-  app.runKernel();
-  
-  //Write previous WebCL call to output.
-  leftChannel = e.outputBuffer.getChannelData(0);
-  rightChannel = e.outputBuffer.getChannelData(1);
-  
-  if (app.rain.bOutputFirstBuffer) {
-    
-    for (i = 0; i < app.rain.timestep; i += 1) {
-      leftChannel[i] = app.rain.firstBuffer[i];
-      rightChannel[i] = app.rain.firstBuffer[i + app.rain.timestep];
-      
-      //Blank the buffer behind it. Read without write -> silence.
-      app.rain.firstBuffer[i] = 0;
-      app.rain.firstBuffer[i + app.rain.timestep] = 0;
-    }
-    
-  } else {
-    
-    for (i = 0; i < app.rain.timestep; i += 1) {
-      leftChannel[i] = app.rain.secondBuffer[i];
-      rightChannel[i] = app.rain.secondBuffer[i + app.rain.timestep];
-      
-      //Blank the buffer behind it. Read without write -> silence.
-      app.rain.secondBuffer[i] = 0;
-      app.rain.secondBuffer[i + app.rain.timestep] = 0;
-    }
-    
-  }
-  
-  //Swap buffers for next run
-  app.rain.bOutputFirstBuffer = !app.rain.bOutputFirstBuffer;
-};
-
-app.runKernel = function () {
+//Runs when number of particles are selected, just after play pressed.
+app.primeWebCL = function () {
   "use strict";
   
-  //cl = app.clGetter.clCtx;
-  app.kernel.setArg(4, new window.Float32Array([(app.boid.location.x / app.elMap.clientWidth) * app.webclDistanceScale, (app.boid.location.y / app.elMap.clientHeight) * app.webclDistanceScale]));
-  app.kernel.setArg(5, new window.Float32Array([app.boid.direction.x, app.boid.direction.y]));
   
-  //app.cmdQueue = cl.createCommandQueue(app.device);
-  //var time = window.performance.now();
-  app.cmdQueue.enqueueWriteBuffer(app.bufParticlesFloat, false, 0, app.rain.webclCallsFloat.byteLength, app.rain.webclFloatView);
-  app.cmdQueue.enqueueWriteBuffer(app.bufParticlesInt, false, 0, app.rain.webclCallsInt.byteLength, app.rain.webclIntView);
-  
-  app.cmdQueue.enqueueNDRangeKernel(app.kernel, 1, null, [app.rain.timestep]);
-  
-  //output = new window.Float32Array(app.rain.timestep * 2);
-  //app.cmdQueue.enqueueReadBuffer(app.bufOutput, false, 0, app.output.byteLength, app.output);
-  //console.log(window.performance.now() - time);
-  //app.cmdQueue.enqueueReadBuffer(app.bufOutput, false, 0, app.rain.timestep * 2, output);
-  
-  //return app.output;
 };
 
 //region Window Event Listeners
